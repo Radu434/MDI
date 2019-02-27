88776309b98aefc2e6eb6aa0b0850c315a5efa8c
Radu434 <47888258+Radu434@users.noreply.github.com>
2019-02-27 12:02:11 +0200

    Add files via upload

---
+++ mdi/mdi/frmMain.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace mdi
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Meniu = new System.Windows.Forms.MenuStrip();
+            this.masiniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cautareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adaugareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.modificareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stergereToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Meniu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Meniu
+            // 
+            this.Meniu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.masiniToolStripMenuItem});
+            this.Meniu.Location = new System.Drawing.Point(0, 0);
+            this.Meniu.Name = "Meniu";
+            this.Meniu.Size = new System.Drawing.Size(578, 24);
+            this.Meniu.TabIndex = 1;
+            this.Meniu.Text = "menuStrip1";
+            // 
+            // masiniToolStripMenuItem
+            // 
+            this.masiniToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cautareToolStripMenuItem,
+            this.adaugareToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.modificareToolStripMenuItem,
+            this.stergereToolStripMenuItem});
+            this.masiniToolStripMenuItem.Name = "masiniToolStripMenuItem";
+            this.masiniToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.masiniToolStripMenuItem.Text = "Masini";
+            // 
+            // cautareToolStripMenuItem
+            // 
+            this.cautareToolStripMenuItem.Name = "cautareToolStripMenuItem";
+            this.cautareToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cautareToolStripMenuItem.Text = "Cautare";
+            this.cautareToolStripMenuItem.Click += new System.EventHandler(this.cautareToolStripMenuItem_Click);
+            // 
+            // adaugareToolStripMenuItem
+            // 
+            this.adaugareToolStripMenuItem.Name = "adaugareToolStripMenuItem";
+            this.adaugareToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.adaugareToolStripMenuItem.Text = "Adaugare";
+            this.adaugareToolStripMenuItem.Click += new System.EventHandler(this.adaugareToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // modificareToolStripMenuItem
+            // 
+            this.modificareToolStripMenuItem.Name = "modificareToolStripMenuItem";
+            this.modificareToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.modificareToolStripMenuItem.Text = "Modificare";
+            this.modificareToolStripMenuItem.Click += new System.EventHandler(this.modificareToolStripMenuItem_Click);
+            // 
+            // stergereToolStripMenuItem
+            // 
+            this.stergereToolStripMenuItem.Name = "stergereToolStripMenuItem";
+            this.stergereToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.stergereToolStripMenuItem.Text = "Stergere";
+            this.stergereToolStripMenuItem.Click += new System.EventHandler(this.stergereToolStripMenuItem_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 321);
+            this.Controls.Add(this.Meniu);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.Meniu;
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Meniu.ResumeLayout(false);
+            this.Meniu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip Meniu;
+        private System.Windows.Forms.ToolStripMenuItem masiniToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cautareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adaugareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem modificareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stergereToolStripMenuItem;
+    }
+}
+
---
+++ mdi/mdi/frmMain.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mdi
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public frmCautare fCautare;
+        public frmAdaugare fAdaugare;
+        public frmModificare fModificare;
+        public frmStergere fStergere;
+
+        private void cautareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fCautare = new frmCautare();
+            fCautare.MdiParent = this;
+            fCautare.Show();
+        }
+
+        private void adaugareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fAdaugare = new frmAdaugare();
+            fAdaugare.MdiParent = this;
+            fAdaugare.Show();
+        }
+
+        private void modificareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fModificare = new frmModificare();
+            fModificare.MdiParent = this;
+            fModificare.Show();
+        }
+
+        private void stergereToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fStergere = new frmStergere();
+            fStergere.MdiParent = this;
+            fStergere.Show();
+        }
+    }
+}
